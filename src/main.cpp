0d5b6747b374e7b843c73fd7b4ac7ee805bb450f
Bryceyoung <bryce@Bryceyoungs-MacBook-Pro.local>
2025-08-26 07:34:39 -0600

    merged main

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,4 @@ using mat_t = size_t;
 
 int main()
 {
-   HeapMatrixState<mat_t, 50000, 50000> matrix(10);
-
-   Timer t;
-   matrix.Map(
-      [](mat_t& value, size_t idx)
-      {
-         value = idx;
-      });
-
-   size_t sum = 0;
-   matrix.Fold(sum, [](size_t& currentSum, mat_t value)
-   {
-      currentSum += value;
-   });
-
-   std::cout << t.Milliseconds() << "\n";
-   std::cout << sum << "\n";
 }
